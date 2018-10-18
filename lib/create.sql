0ee8ab4e77bfc268e9b6bf59644f92981f9194a1
Sherry Martin <sm674@cornell.edu>
2018-10-18 18:55:31 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE projects (
+id INTEGER PRIMARY KEY,
+title TEXT,
+category TEXT,
+funding goal INTEGER,
+start date INTEGER,
+end date INTEGER);
+
+
+Users
+name and an age
+
+
+
+Pledges
+ledge has an amount. It belongs to a user, and it also belongs to a project.
